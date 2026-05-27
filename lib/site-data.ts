0c9706ded3765bf19f33e9badaeaca7e43b8b028
김선우 <sunk0419@korea.ac.kr>
2026-05-27 18:10:42 +0900

    0527-8

--- lib/site-data.ts
+++ lib/site-data.ts
@@ -301,7 +301,7 @@ export const pageInfoMap: Record<string, PageInfo> = {
 
 export const partnerLogos = [
   { key: "korea", src: "/partners/korea.svg", alt: "고려대학교 로고" },
-  { key: "yuksa", src: "/partners/yuksa.svg", alt: "육사 로고" },
+  { key: "yuksa", src: "/partners/sugang.png", alt: "서강대 로고" },
   { key: "kriss", src: "/partners/kriss.png", alt: "KRISS 로고" },
   { key: "unist", src: "/partners/unist.png", alt: "UNIST 로고" },
   { key: "chosun", src: "/partners/chosun.svg", alt: "조선대학교 로고" },
